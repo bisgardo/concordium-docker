02a2207a54a9c2351bc62eb756ebc858b3d93125
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-02-18 00:38:24 +0100

    Fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 set -euxo pipefail
 
 docker-compose --env-file="./${1}.env" pull # prevent 'up' from building instead of pulling
-NODE_NAME=qwerty docker-compose --env-file="./${1}.env" up --no-build
+docker-compose --env-file="./${1}.env" up --no-build
