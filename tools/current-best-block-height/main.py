c07b40c8ab3b806729fd18774a8be44e4c9f2d32
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-03-13 16:31:05 +0100

    Add threshold for filtering out dead nodes

--- tools/current-best-block-height/main.py
+++ tools/current-best-block-height/main.py
@@ -3,8 +3,9 @@
 import os
 from query import query
 
-url = os.environ['URL']
+url = os.getenv('URL')
+threshold = int(os.getenv('THRESHOLD', '0'))
 
 if __name__ == '__main__':
-    res = query(url)
+    res = query(url, threshold)
     print(res)
--- tools/current-best-block-height/query.py
+++ tools/current-best-block-height/query.py
@@ -2,9 +2,10 @@ import json
 from statistics import median
 from urllib.request import urlopen
 
-def query(url):
+def query(url, threshold):
     with urlopen(url) as res:
          res_str = res.read()
     res_json = json.loads(res_str)
     best_block_heights = [ r.get('bestBlockHeight', 0) for r in res_json ]
+    best_block_heights = [ h for h in best_block_heights if h >= threshold ]
     return int(median(best_block_heights))
