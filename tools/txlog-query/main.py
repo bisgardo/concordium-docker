d8931ec7bf00ddca4c8cd9589a48a412c368d6ba
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-05-02 21:41:34 +0200

    txlog-query: Support account input in hex

--- tools/txlog-query/main.py
+++ tools/txlog-query/main.py
@@ -4,13 +4,22 @@ from translate_address import *
 from query import *
 import sys, os
 
+
+def address_to_bytes(a):
+    # Attempt to parse as Base58Check, falling back to hex (with optional prefix).
+    try:
+        return bytes_from_base58check(a)
+    except:
+        return bytes.fromhex(a[a.find('x')+1:]) # strip any prefix ending with 'x'
+
+
 if __name__ == '__main__':
     host = os.getenv('PGHOST', 'localhost')
     port = int(os.getenv('PGPORT', '5432'))
     database = os.getenv('PGDATABASE', 'concordium_txlog')
     user = os.getenv('PGUSER', 'postgres')
     password = os.getenv('PGPASSWORD')
-    account_address = sys.argv[1]
+    account_address_bytes = address_to_bytes(sys.argv[1])
 
     connection = connect(
         host=host,
@@ -20,8 +29,7 @@ if __name__ == '__main__':
         password=password,
     )
 
-    rows = query_by_address_bytes(connection, bytes_from_base58check(account_address))
-    for (address_bytes, block_bytes, timestamp, height, summary_json) in rows:
-        # Round-trip sanity check.
-        assert (base58check_from_bytes(address_bytes) == account_address)
+    rows = query_by_address_bytes(connection, account_address_bytes)
+    for address, block, timestamp, height, summary_json in rows:
+        assert address.tobytes() == account_address_bytes
         print(summary_json)
