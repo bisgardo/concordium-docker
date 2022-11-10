206f98e9e521cc9144134cf2ac913fa99216c2fd
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2022-11-10 22:01:00 +0100

    Work

--- tools/benchmark-compression/main.py
+++ tools/benchmark-compression/main.py
@@ -13,20 +13,21 @@ import time
 
 def run(out, source_dir, tmp_base_dir, source_hash, archive_ext):
     start_time = time.time()
-    logging.debug('Running "%s" with ext "%s"', source_dir, archive_ext)
+    logging.info('Running "%s" with ext "%s"', source_dir, archive_ext)
     tmp_dir = tempfile.mkdtemp(dir=tmp_base_dir)
     logging.debug('Using temporary directory "%s"', tmp_dir)
     archive_file = os.path.join(tmp_base_dir, f'data.tar.{archive_ext}')
     try:
-        logging.debug('Compressing "%s" into "%s"', source_dir, archive_file)
+        logging.info('Compressing "%s" into "%s"', source_dir, archive_file)
         compression_time = compress(source_dir, archive_file)
         archive_size = os.path.getsize(archive_file)
-        logging.debug('Compression time:  %.2f s', compression_time)
-        logging.debug('Compressed size:   %d B', archive_size)
-        logging.debug('Decompressing "%s" into "%s"', archive_file, tmp_dir)
+        logging.info('Compression time:  %.2f s', compression_time)
+        logging.info('Compressed size:   %d B', archive_size)
+        logging.info('Decompressing "%s" into "%s"', archive_file, tmp_dir)
         decompression_time = decompress(archive_file, tmp_dir)
+        logging.info('Decompression time: %.2f s', decompression_time)
+        logging.debug('Computing hash of decompressed directories')
         target_hash = checksumdir.dirhash(source_dir)
-        logging.debug('Decompression time: %.2f s', decompression_time)
         out.writerow({
             'start_time': start_time,
             'archive_ext': archive_ext,
@@ -36,6 +37,7 @@ def run(out, source_dir, tmp_base_dir, source_hash, archive_ext):
             'source_hash': source_hash,
             'target_hash': target_hash,
         })
+        logging.info('Done')
     finally:
         shutil.rmtree(tmp_dir)
         try:
@@ -45,7 +47,7 @@ def run(out, source_dir, tmp_base_dir, source_hash, archive_ext):
 
 
 def compress(source_dir, target_file, flag='--auto-compress'):
-    return run_timed(f'tar --create "{flag}" --file="{target_file}" "{source_dir}"')
+    return run_timed(f'tar --create {flag} --file="{target_file}" "{source_dir}"')
 
 
 def decompress(source_file, target_dir):
@@ -70,19 +72,35 @@ logging.basicConfig(level=log_level)
 if __name__ == '__main__':
     out = sys.stdout
     if source_hash is None:
-        logging.debug('Computing hash of original data')
+        logging.info('Computing hash of original data')
         source_hash = checksumdir.dirhash(source_dir, hashfunc='md5')
-        logging.debug('Hash of original data is %s', source_hash)
+        logging.info('Hash of original data is %s', source_hash)
     logging.debug('Initializing writer')
     header = ['start_time', 'archive_ext', 'archive_size', 'compression_time', 'decompression_time', 'source_hash', 'target_hash']
     writer = csv.DictWriter(out, delimiter=',', fieldnames=header)
     writer.writeheader()
 
-    logging.debug('Starting measurements')
+    logging.info('Starting measurements')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'tar')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'tar')
     run(writer, source_dir, tmp_base_dir, source_hash, 'gz')
     run(writer, source_dir, tmp_base_dir, source_hash, 'gz')
     run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
     run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
-    run(writer, source_dir, tmp_base_dir, source_hash, 'bzip2')
-    run(writer, source_dir, tmp_base_dir, source_hash, 'bzip2')
-    logging.debug('Done')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'bz2')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'bz2')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lz')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lz')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lzma')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lzma')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lzo')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lzo')
+    os.environ['XZ_DEFAULTS'] = '-T0'
+    run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'lzma')
+    os.environ['XZ_DEFAULTS'] = '-T0 -9'
+    run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
+    run(writer, source_dir, tmp_base_dir, source_hash, 'xz')
+
+    logging.info('All done')
