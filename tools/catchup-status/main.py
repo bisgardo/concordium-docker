0839f0d0382f8686133ea9cbb0f9a2056fd3fb83
Michael Bisgaard Olesen <michaelbisgaardo@gmail.com>
2023-09-25 13:02:55 +0200

    Add tool for resolving catchup status for a given node
    The tool currently just prints the block height of the chain relative to the provided node. The node is provided by name and the chain's current height is determined by a given percentile of the reported heights for the nodes that are deemed up to date. The nodes that are deemed up to date is again determined as a percentile of the reporting nodes' reported versions.
    
    There are a number of possible improvements to be made mentioned in TODOs in the file.

---
+++ tools/catchup-status/main.py
@@ -0,0 +1,67 @@
+from pydantic import BaseModel
+from pydantic.functional_validators import PlainValidator
+from typing_extensions import Annotated
+
+import requests
+import argparse
+
+# Parse CLI args.
+# TODO: Add ability to select version rather than percentile? And default to version of tracked node?
+#       Might also allow different percentiles for version lookup vs block lookup.
+parser = argparse.ArgumentParser(prog='catchup-status', description='Status of catchup')
+parser.add_argument('-p', '--percentile', type=int, default=90)
+parser.add_argument('-u', '--url', type=str, default='https://dashboard.mainnet.concordium.software/nodesSummary')
+parser.add_argument('node', type=str)
+args = parser.parse_args()
+
+# Use same settings for both percentiles for now.
+version_percentile = args.percentile / 100
+block_height_percentile = args.percentile / 100
+node_name = args.node
+url = args.url
+
+
+# Set up model for data to be fetched.
+def parse_version(c):
+    [major, minor, patch] = c.split('.')
+    return (major, minor, patch)
+
+
+class NodeSummary(BaseModel):
+    nodeName: str
+    client: PlainValidator(parse_version)
+    bestBlockHeight: int
+
+
+# Fetch and decode JSON.
+r = requests.get(url)
+# TODO: Check status code.
+summaries = [NodeSummary(**s) for s in r.json()]
+#print(len(summaries))
+
+# TODO: Validate...
+#       ...and/or allow multiple nodes to be tracked?
+node = [s for s in summaries if s.nodeName == node_name][0]
+#print('node', node)
+
+# Resolve version for the provided percentile.
+sorted_versions = sorted([s.client for s in summaries])
+version_percentile_rank = int(version_percentile * (len(sorted_versions)-1)) # we don't need perfect accuracy
+version = sorted_versions[version_percentile_rank]
+#print(version)
+
+# Filter summaries based on resolved version (including newer versions - should we only match by exact version??).
+valid_summaries = [s for s in summaries if s.client >= version]
+
+# Resolve "current" best block height for the provided percentile amongst the "valid" nodes.
+sorted_block_heights = sorted([s.bestBlockHeight for s in summaries])
+block_height_percentile_rank = int(version_percentile * (len(sorted_versions)-1)) # we don't need perfect accuracy
+block_height = sorted_block_heights[block_height_percentile_rank]
+
+node_block_height = node.bestBlockHeight
+node_block_height_pct = (node_block_height/block_height)*100
+
+print(f'Current height: {block_height}')
+print(f'Node height   : {node_block_height} ({node_block_height_pct:.1f}%)')
+
+# TODO: Run the script periodically and use the recorded progress to give an esimate of the time remaining before the node is fully caught up (track the progress in percentage, not number of blocks).
